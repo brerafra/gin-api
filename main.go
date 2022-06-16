86a67d29308cd3d5f799a1f2abca159232a7be0e
brerafra <brerafra@gmail.com>
2022-06-16 16:41:49 -0500

    version final

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 // album represents data about a record album
@@ -20,6 +22,13 @@ var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+//User struct
+type User struct {
+	gorm.Model
+	Name  string `json:"name"`
+	Email string `json:"email"`
 }
 
 //postAlbums adds an album from josn received in the request body
@@ -58,9 +67,63 @@ func getAlbums(c *gin.Context) {
 }
 
 func main() {
+	fmt.Println("gorm-gin usos")
+	handleRequsts()
+}
+
+func handleRequsts() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
 	router.POST("/albums", postAlbums)
 	router.GET("/albums/:id", getAlbumByID)
+
+	router.GET("/users", allUsers)
+	router.POST("/users", newUser)
+	initialMigration()
+
 	router.Run("localhost:8080")
+}
+
+func allUsers(c *gin.Context) {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	var users []User
+
+	db.Find(&users)
+	fmt.Println("{}", users)
+	c.IndentedJSON(http.StatusOK, users)
+}
+
+func newUser(c *gin.Context) {
+	var newUser User
+	//Call Bindjson to bind teh received JSON to newAlbum,
+	if err := c.BindJSON(&newUser); err != nil {
+		return
+	}
+	fmt.Println(newUser)
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	db.Create(&newUser)
+
+	c.IndentedJSON(http.StatusCreated, newUser)
+}
+
+func initialMigration() {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("falied to conect database")
+	}
+	defer db.Close()
+
+	//Migrate the schema
+	db.AutoMigrate(&User{})
 }
